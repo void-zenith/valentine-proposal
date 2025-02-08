edb8adef1c15498f725f1251b8905f2979ffbff1
void-zenith <zenithrajbhandari@gmail.com>
2025-02-08 04:51:55 -0500

    updated

--- src/Components/Accepted.jsx
+++ src/Components/Accepted.jsx
@@ -4,7 +4,8 @@ import "../App.css"
 const Accepted = () => {
     return <div className='accepted-container'>
         <img src={img1} alt="us" />
-        <h1>Hey valentine! Thank you for your trust. I cannot wait to go on endless coffee dates with you</h1>
+        <h1>Hey valentine! Thank you for your trust. I cannot wait to go on endless coffee dates with you!!</h1>
+        <p>❤️❤️❤️</p>
     </div>
 }
 export default Accepted;
